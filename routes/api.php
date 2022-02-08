ade6e09e01d11c942e9758f5c002e57275e53f3d
nopri888-anto <79070725+nopri888-anto@users.noreply.github.com>
2022-02-08 09:30:08 +0700

    Update Route and Controller Transaction

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('/transaction', [TransactionController::class, 'index']);
+// Route::post('/transaction', [TransactionController::class, 'store']);
+// Route::put('/transaction/{id}', [TransactionController::class, 'update']);
+// Route::get('/transaction/{id}', [TransactionController::class, 'show']);
+// Route::delete('/transaction/{id}', [TransactionController::class, 'destroy']);
+
+Route::resource('/transaction', TransactionController::class)->except(['created', 'edit']);
